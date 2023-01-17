e18784686523beed7d938454860c6a8a5934813d
lucmac66 <lucas.macias66@gmail.com>
2023-01-17 15:11:46 +0100

    GrpcClient/

---
+++ Multi-Flow-Derivatives-Hedging-Project/GrpcClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcPricing.Protos;
+
+var httpHandler = new HttpClientHandler();
+// Return `true` to allow certificates that are untrusted/invalid
+httpHandler.ServerCertificateCustomValidationCallback =
+	HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+using var channel = GrpcChannel.ForAddress("http://localhost:5079",
+	new GrpcChannelOptions { HttpHandler = httpHandler });
+var client = new GrpcPricer.GrpcPricerClient(channel);
