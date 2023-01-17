8d9960e4fb0f868a9ad69e32da8755b68e289c01
lucmac66 <lucas.macias66@gmail.com>
2023-01-17 15:17:18 +0100

    Update

--- Princing-Engine/GrpcServer/src/pricing_server.cpp
+++ Princing-Engine/GrpcServer/src/pricing_server.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <memory>
 #include <string>
+#include <fstream>
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
+#include <nlohmann/json.hpp>
 
 #include "pnl/pnl_matrix.h"
 #include "pricing.grpc.pb.h"
+#include "pricer.hpp"
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -43,32 +45,45 @@ PnlMat* convertPastToPnlMat(const PricingInput *input) {
 // Logic and data behind the server's behavior.
 class GrpcPricerImpl final : public GrpcPricer::Service {
 public:
-    GrpcPricerImpl() {}
+    BlackScholesPricer &pricer;
+    GrpcPricerImpl(BlackScholesPricer &pricer) : pricer(pricer) {}
 
-    Status Price(ServerContext *context, const PricingInput *input, PricingOutput *output) override {
-        int index = input->index();
-        bool monitoringDateReached = input->monitoringdatereached();
-        double current_time = input->time();
+    Status PriceAndDeltas(ServerContext *context, const PricingInput *input, PricingOutput *output) override {
+        double price, priceStdDev;
+        PnlVect *delta, *deltaStdDev;
+        bool isMonitoringDate = input->monitoringdatereached();
+        double currentDate = input->time();
         PnlMat *past = convertPastToPnlMat(input);
         if (past == NULL) {
             return Status(grpc::StatusCode::INVALID_ARGUMENT, "Cannot read past");
         }
         pnl_mat_print(past);
-        output->set_price(current_time);
-        output->add_deltas(1.);
-        output->add_deltas(2.);
+        pricer.priceAndDeltas(past, currentDate, isMonitoringDate, price, priceStdDev, delta, deltaStdDev);
+        output->set_price(price);
+        output->set_pricestddev(priceStdDev);
+        for (int i = 0; i < delta->size; i++) {
+            output->add_deltas(GET(delta, i));
+            output->add_deltasstddev(GET(deltaStdDev, i));
+        }
         pnl_mat_free(&past);
+        pnl_vect_free(&delta);
+        pnl_vect_free(&deltaStdDev);
         return Status::OK;
     }
 
-    Status Info(ServerContext *context, const Empty* input, ReqInfo *output) override {
+    Status Heartbeat(ServerContext *context, const Empty* input, ReqInfo *output) override {
+        output->set_domesticinterestrate(pricer.interestRate);
+        output->set_relativefinitedifferencestep(pricer.fdStep);
+        output->set_samplenb(pricer.nSamples);
         return Status::OK;
     }
 };
 
-void RunServer() {
+void RunServer(nlohmann::json &jsonParams) {
     std::string server_address("0.0.0.0:50051");
-    GrpcPricerImpl service;
+    BlackScholesPricer pricer(jsonParams);
+    pricer.print();
+    GrpcPricerImpl service(pricer);
 
     grpc::EnableDefaultHealthCheckService(true);
     grpc::reflection::InitProtoReflectionServerBuilderPlugin();
@@ -76,7 +91,7 @@ void RunServer() {
     // Listen on the given address without any authentication mechanism.
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
     // Register "service" as the instance through which we'll communicate with
-    // clients. In this case it corresponds to an *synchronous* service.
+    // clients. In this case it corresponds to a *synchronous* service.
     builder.RegisterService(&service);
     // Finally assemble the server.
     std::unique_ptr<Server> server(builder.BuildAndStart());
@@ -88,7 +103,13 @@ void RunServer() {
 }
 
 int main(int argc, char **argv) {
-    RunServer();
+    if (argc != 2) {
+        std::cout << "Exactly one argument is required." << std::endl;
+        std::cout << "Usage: ./pricing_server math_params.json" << std::endl;
+    }
+    std::ifstream ifs(argv[1]);
+    nlohmann::json jsonParams = nlohmann::json::parse(ifs);
+    RunServer(jsonParams);
 
     return 0;
 }
